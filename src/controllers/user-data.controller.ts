2388135f4b3b5e668442ab84b3883d3475ccee06
Dexaf <federicopisajob@gmail.com>
2024-01-15 15:30:17 +0100

    changed order in which profile picture get saved in UserData/Profile POST endpoint method

--- src/controllers/user-data.controller.ts
+++ src/controllers/user-data.controller.ts
@@ -7,47 +7,47 @@ import { ErrorExt } from "../models/extensions/error.extension.js";
 import path from "path";
 import { base64MimeType } from "../utils/getBase64MimeType.js";
 import { saveProfilePicture } from "../utils/saveProfilePicture.js";
-import { promises as fsPromises } from "fs";
-
 
 //post profile data
 export const postProfile = async (req: CustomRequest, res: express.Response, next: express.NextFunction) => {
-  let imagePath: string | undefined = undefined;
   try {
     validationHandlingRoutine(req, res);
-    const body = req.body as PostProfileDto;
-
     let user = await UserModel.findById({ _id: req.user!.id })
-
     if (!user) {
       throw new ErrorExt("USERNAME_NO_MATCH", 404);
     }
 
-    let relativeImagePath: string | undefined = undefined;
+    const body = req.body as PostProfileDto;
 
-    if (body.profilePictureBase64) {
-      const imageExtension = base64MimeType(body.profilePictureBase64)
-      const projectRoot = path.resolve(process.cwd());
-      relativeImagePath = `\\public\\profilePictures\\${user.username}.${imageExtension.split("/")[1]}`
-      imagePath = projectRoot + relativeImagePath;
-    }
-
+    //save data but avoid overwrite of empty propr in req body 
     user.name = body.name ?? user.name;
     user.surname = body.surname ?? user.surname;
     user.countryCode = body.countryCode ?? user.countryCode;
     user.description = body.description ?? user.description;
     user.email = body.email ?? user.email;
 
-    if (imagePath && body.profilePictureBase64) {
-      await saveProfilePicture(body.profilePictureBase64, imagePath);
-      user.profilePictureUrl = relativeImagePath ?? user.profilePictureUrl;
+    let relativeImagePath: string | undefined = undefined;
+    let imagePath: string | undefined = undefined;
+
+    if (body.profilePictureBase64) {
+      const imageExtension = base64MimeType(body.profilePictureBase64)
+      relativeImagePath = `\\public\\profilePictures\\${user.username}.${imageExtension.split("/")[1]}`
+      user.profilePictureUrl = relativeImagePath;
     }
+
     await user.save();
 
+    /* NOTE - we take for granted that local file writes works so 
+              we are not going to implement any rollback function 
+              for the case of succesful user insert and failed picture save.
+    */
+    if (body.profilePictureBase64) {
+      const projectRoot = path.resolve(process.cwd());
+      imagePath = projectRoot + relativeImagePath;
+      await saveProfilePicture(body.profilePictureBase64, imagePath);
+    }
     return res.status(200).send();
   } catch (error: any) {
-    if (imagePath)
-      fsPromises.rm(imagePath)
     return errorHandlingRoutine(error, next);
   }
 }

9d29ab1a37ba710ef93c107c139ad7b9449027ec
Dexaf <federicopisajob@gmail.com>
2024-01-18 01:15:58 +0100

    added recipes endpoints

--- src/controllers/recipes.controller.ts
+++ src/controllers/recipes.controller.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { promises as fsPromises } from 'fs';
+import path from "path";
 
 import { CustomRequest } from "../models/extensions/request.extension.js";
 import { errorHandlingRoutine, validationHandlingRoutine } from "../utils/errorHandlingRoutines.js";
@@ -9,7 +10,6 @@ import { UserModel } from "../models/schemas/user.schema.js";
 import { ErrorExt } from "../models/extensions/error.extension.js";
 import { RecipeModel } from "../models/schemas/recipes.schemas.js";
 import { base64MimeType } from "../utils/getBase64MimeType.js";
-import path from "path";
 import { savePicture } from "../utils/savePicture.js";
 
 export const addRecipe = async (req: CustomRequest, res: express.Response, next: express.NextFunction) => {
@@ -18,7 +18,7 @@ export const addRecipe = async (req: CustomRequest, res: express.Response, next:
 
     const user = await UserModel.findById(req.user!.id);
     if (!user)
-      throw new ErrorExt("USERNAME_NO_MATCH", 404);
+      throw new ErrorExt("USER_NO_MATCH", 404);
 
     const body: AddRecipeDtoReq = req.body;
 
@@ -68,5 +68,81 @@ export const addRecipe = async (req: CustomRequest, res: express.Response, next:
     res.status(200).send();
   } catch (error: any) {
     errorHandlingRoutine(error, next);
+  }
+}
+
+export const getOwnRecipes = async (req: CustomRequest, res: express.Response, next: express.NextFunction) => {
+  try {
+    validationHandlingRoutine(req);
+
+    const user = await UserModel.findById(req.user!.id);
+    if (!user)
+      throw new ErrorExt("USER_NO_MATCH", 404);
+
+    const recipes = await RecipeModel.find({ userId: user._id });
+    res.json(recipes).status(200).send();
+  } catch (error) {
+    errorHandlingRoutine(error, next)
+  }
+}
+
+export const getOwnRecipe = async (req: CustomRequest, res: express.Response, next: express.NextFunction) => {
+  try {
+    validationHandlingRoutine(req);
+
+    const user = await UserModel.findById(req.user!.id);
+    if (!user)
+      throw new ErrorExt("USER_NO_MATCH", 404);
+
+    const recipeId = req.params.recipeId;
+
+    const recipe = await RecipeModel.findOne({ $and: [{ userId: user._id }, { _id: recipeId }] });
+    res.json(recipe).status(200).send();
+  } catch (error) {
+    errorHandlingRoutine(error, next)
+  }
+}
+
+export const getUserRecipes = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    validationHandlingRoutine(req);
+    const userId = req.params.userId;
+
+    const recipes = await RecipeModel.find({ userId: userId });
+    res.json(recipes).status(200).send();
+  } catch (error) {
+    errorHandlingRoutine(error, next);
+  }
+}
+
+export const getUserRecipe = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    validationHandlingRoutine(req);
+    const userId = req.params.userId;
+    const recipeId = req.params.recipeId;
+
+    const recipe = await RecipeModel.findOne({ $and: [{ userId: userId }, { _id: recipeId }] });
+    res.json(recipe).status(200).send();
+  } catch (error) {
+    errorHandlingRoutine(error, next);
+  }
+}
+
+export const deleteOwnRecipe = async (req: CustomRequest, res: express.Response, next: express.NextFunction) => {
+  try {
+    validationHandlingRoutine(req);
+    const user = await UserModel.findById(req.user!.id);
+    if (!user)
+      throw new ErrorExt("USER_NO_MATCH", 404);
+
+    const recipeId = req.params.recipeId;
+    const isDeleted = await RecipeModel.deleteOne({ $and: [{ _id: recipeId }, { userId: user._id }] })
+    if (isDeleted.deletedCount === 0)
+      throw new ErrorExt("RECIPE_NO_MATCH", 404)
+    else
+      res.status(204).send()
+  } catch (error) {
+    errorHandlingRoutine(error, next);
+
   }
 }
--- src/routes/recipes.routes.ts/recipes.routes.ts
+++ src/routes/recipes.routes.ts/recipes.routes.ts
@@ -7,16 +7,16 @@ const recipesRouter = express.Router();
 //add new recipe
 recipesRouter.post('/Add', isAuthGuard, recipeValidationChains.addRecipe, recipesController.addRecipe);
 //get all recipe of authenticated account
-recipesRouter.get('/Own', isAuthGuard);
+recipesRouter.get('/Own', isAuthGuard, recipesController.getOwnRecipes);
 //get a certain recipe of authenticated account
-recipesRouter.get('/Own/:recipeId', isAuthGuard);
+recipesRouter.get('/Own/:recipeId', isAuthGuard, recipesController.getOwnRecipe);
 //get all recipe of an account
-recipesRouter.get('/:userId/');
+recipesRouter.get('/:userId/', recipesController.getUserRecipes);
 //get a certain recipe of an account
-recipesRouter.get('/:userId/:recipeId');
+recipesRouter.get('/:userId/:recipeId', recipesController.getUserRecipe);
 //update a certain recipe of an account
-recipesRouter.patch('/:recipeId', isAuthGuard);
+recipesRouter.patch('/:recipeId', isAuthGuard); //TODO - update only first layer of doc, leave ingrd and steps for other end points
 //delete certains recipes of authenticated account 
-recipesRouter.delete('/:recipeId', isAuthGuard);
+recipesRouter.delete('/:recipeId', isAuthGuard, recipesController.deleteOwnRecipe);
 
 export default recipesRouter;

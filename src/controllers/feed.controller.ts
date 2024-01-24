27c09bcd8ea62b20d2541ea124a84d7314e29632
Dexaf <federicopisajob@gmail.com>
2024-01-24 01:09:22 +0100

    refactored suggested search var names

--- src/controllers/feed.controller.ts
+++ src/controllers/feed.controller.ts
@@ -15,31 +15,31 @@ export const searchSuggestion = async (req: express.Request, res: express.Respon
     const searchInput = query.searchInput;
     const includeQuarters = query.includeQuarters;
 
-    const recipesRet: string[] = []
+    const suggestedTitles: string[] = []
 
-    const recipes = await RecipeModel.find({}).select("-_id " + getfieldName<Recipe>("title"))
+    const titles = (await RecipeModel.find({}).select("-_id " + getfieldName<Recipe>("title"))).map(r => { return r.title.toLowerCase() })
 
-    const perfectMatch: string[] = [];
+    const fullMatch: string[] = [];
     const halfMatch: string[] = [];
     const threeQuarteMatch: string[] = [];
 
-    for (const recipe of recipes) {
-      if (recipe.title.includes(searchInput))
-        perfectMatch.push(recipe.title)
+    for (const title of titles) {
+      if (title.includes(searchInput))
+        fullMatch.push(title)
       else if (includeQuarters) {
-        if (searchInput.length > 2 && recipe.title.includes(searchInput.substring(0, Math.ceil((searchInput.length / 4) * 2))))
-          halfMatch.push(recipe.title)
-        else if (searchInput.length > 4 && recipe.title.includes(searchInput.substring(0, Math.ceil((searchInput.length / 4) * 3))))
-          threeQuarteMatch.push(recipe.title)
+        if (searchInput.length > 2 && title.includes(searchInput.substring(0, Math.ceil((searchInput.length / 4) * 2))))
+          halfMatch.push(title)
+        else if (searchInput.length > 4 && title.includes(searchInput.substring(0, Math.ceil((searchInput.length / 4) * 3))))
+          threeQuarteMatch.push(title)
       }
 
-      if ((perfectMatch.length + halfMatch.length + threeQuarteMatch.length) > 9)
+      if ((fullMatch.length + halfMatch.length + threeQuarteMatch.length) > 9)
         break;
     }
 
-    recipesRet.push(...perfectMatch, ...halfMatch, ...threeQuarteMatch);
-    const status = recipesRet.length > 0 ? 200 : 204;
-    res.status(status).send(recipesRet);
+    suggestedTitles.push(...fullMatch, ...halfMatch, ...threeQuarteMatch);
+    const status = suggestedTitles.length > 0 ? 200 : 204;
+    res.status(status).send(suggestedTitles);
   } catch (error) {
     errorHandlingRoutine(error, next);
   }
